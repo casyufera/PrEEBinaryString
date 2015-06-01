50d4476eb45e0db71650bd8f06bf2be6b860a1e6
casyufera <casyufera@icloud.com>
2015-06-02 01:45:26 +0200

    PruebaEEBS1

---
+++ PruebaEE4.java
@@ -0,0 +1,32 @@
+package paqueteEjemploExamen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class PruebaEE4 {
+	
+	//Apartado 4: Implementar testSetOutOfLimitBit()
+
+	private BinaryString bitset1, bitset2, bitset3, bitset4;
+	private String pruebaString2 = "1010101010", pruebaString3 = "100101", pruebaString4 = "0110101";
+	
+	//Inicializamos las variables bitset1 con su String asociado
+	@Before
+	public void antesDelTest() {
+		bitset1 = new BinaryString(10);
+		bitset2 = new BinaryString(pruebaString2);
+		bitset3 = new BinaryString(pruebaString3);
+		bitset4 = new BinaryString(pruebaString4);
+	}
+
+	//Comprobamos si al acceder fuera de los límites del array nos lanza una excepción tipo BinaryStringException
+	//pero utilizando el método set()
+	@Test(expected = BinaryStringException.class)
+	public void testSetOutOfLimitBit() {
+		bitset4.set(56, '0');
+	}
+}
+
+
