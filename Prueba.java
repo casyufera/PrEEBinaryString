b760cddd12b0938815c636b668ec9b18b9a0759a
casyufera <casyufera@icloud.com>
2015-06-02 02:31:11 +0200

    Prueba Ap7

--- Prueba.java
+++ Prueba.java
@@ -7,10 +7,10 @@ import org.junit.Before;
 
 public class Prueba {
 	
-	//Apartado 6: Implementar testInvalidBitValue()
+	//Apartado 7: Implementar testToString()
 
-	private BinaryString bitset1, bitset2, bitset3, bitset4;
-	private String pruebaString2 = "1010101010", pruebaString3 = "100101", pruebaString4 = "0110101";
+	private BinaryString bitset1, bitset2, bitset3, bitset4, bitset5;
+	private String pruebaString2 = "1010101010", pruebaString3 = "100101", pruebaString4 = "0110101", pruebaString5 = "1111000";
 	
 	//Inicializamos las variables bitset1 con su String asociado
 	@Before
@@ -19,12 +19,13 @@ public class Prueba {
 		bitset2 = new BinaryString(pruebaString2);
 		bitset3 = new BinaryString(pruebaString3);
 		bitset4 = new BinaryString(pruebaString4);
+		bitset5 = new BinaryString(pruebaString5);
 	}
 
-	//Comprobamos si al intentar meter un char que no sea '1' o '0' lanza una excepción BinaryStringException 
-	@Test(expected = BinaryStringException.class)
-	public void testInvalidBitValue() {
-			bitset4.set(1, 'a');
+	//Comprobamos que al intentar toString el resultado es el esperado   
+	@Test
+	public void testToString() {
+			assertEquals("1111000", bitset5.toString());
 	}
 }
 
