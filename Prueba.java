3e171ab9d8270bbd96c42e96202cbecfe6a971a6
casyufera <casyufera@icloud.com>
2015-06-02 02:24:40 +0200

    Prueba Ap2

--- Prueba.java
+++ Prueba.java
@@ -7,22 +7,24 @@ import org.junit.Before;
 
 public class Prueba {
 	
-	//Apartado 1: Implementar testLength()
+	//Apartado 2: Implementar testSetBit()
 
-	private BinaryString bitset1, bitset2;
-	private String pruebaString2 = "1010101010";
+	private BinaryString bitset1, bitset2, bitset3;
+	private String pruebaString2 = "1010101010", pruebaString3 = "100101";
 	
 	//Inicializamos las variables bitset1 con su String asociado
 	@Before
 	public void antesDelTest() {
 		bitset1 = new BinaryString(10);
 		bitset2 = new BinaryString(pruebaString2);
+		bitset3 = new BinaryString(pruebaString3);
 	}
 
-	//Comprobamos si al inicializar el bitset el tamaño es el esperado
+	//Comprobamos si al cambiar una posición del bitset a un '1' el cambio se hace efectivo
 	@Test
-	public void testLength() {
-		assertEquals (10, bitset1.length());
-		assertEquals (10, bitset2.length());
+	public void testSetBit() {
+		bitset3.set(1,'1');
+		assertEquals('1', bitset3.get(1));
 	}
 }
+
